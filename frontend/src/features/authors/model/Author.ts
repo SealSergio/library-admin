c82aa304d81b2b99abae89f66b643eb4ca5ac0d3
SealSergio <sergiobogunio@gmail.com>
2025-12-17 20:03:19 +0300

    refactor[/model/Authors.ts] изменить AuthorsSchema
    Удалено значение "Country"

---
+++ frontend/src/features/authors/model/Author.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const AuthorSchema = z.object({
+    id: z.string(),
+    surname: z.string(),
+    name: z.string(),
+    secondName: z.string().optional(),
+    fullname: z.string(),
+    abbreviatedName: z.string(),
+});
+
+export type Author = z.infer<typeof AuthorSchema>;
+
+export const AuthorList = z.array(AuthorSchema);
+
+export type AuthorList = z.infer<typeof AuthorList>;
+
+export type AuthorData = Author | null;
